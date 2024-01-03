a4df9a9f6ee9a3487f2f3d331e05bbf3b98d5227
pascallino <luvpascal.ojukwu@yahoo.com>
2024-01-03 10:56:26 +0100

    3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,10 +3,9 @@ import { createUser, uploadPhoto } from './utils';
 export default function handleProfileSignup() {
   const promise1 = uploadPhoto();
   const promise2 = createUser();
-  /* eslint-disable no-unused-vars */
   Promise.all([promise1, promise2])
     .then((results) => {
       console.log(`${results[0].body} ${results[1].firstName} ${results[1].lastName}`);
     })
-    .catch((error) => { console.log('Signup system offline'); });
+    .catch(() => { console.log('Signup system offline'); });
 }
