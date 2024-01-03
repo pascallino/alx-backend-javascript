68524e585c106458322017877cce79f597612d55
pascallino <luvpascal.ojukwu@yahoo.com>
2024-01-03 09:59:12 +0100

    2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -3,7 +3,7 @@ export default function handleResponseFromAPI(promise) {
   return promise.then((result) => {
     console.log('Got a response from the API');
     return { status: 200, body: 'Success' };
-  }).catch((error) => {
+  }, (error) => {
     console.log('Got a response from the API');
     return new Error();
   });
