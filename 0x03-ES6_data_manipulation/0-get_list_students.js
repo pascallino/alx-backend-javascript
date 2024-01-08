f2a23f81cc67839e970c4adc2d46e374e11d7200
pascallino <luvpascal.ojukwu@yahoo.com>
2024-01-08 15:39:51 +0100

    0-get_list_students.js

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,7 @@
+export default function getListStudents() {
+  const array = [];
+  array.push({ id: 1, firstName: 'Guillaume', location: 'San Francisco' });
+  array.push({ id: 2, firstName: 'James', location: 'Columbia' });
+  array.push({ id: 5, firstName: 'Serena', location: 'San Francisco' });
+  return array;
+}
