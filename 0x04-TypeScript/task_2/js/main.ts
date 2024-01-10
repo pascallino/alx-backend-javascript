30876ce843893b496759eb29d646d00f2191cfb2
pascallino <luvpascal.ojukwu@yahoo.com>
2024-01-10 19:06:28 +0100

    js/

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -59,3 +59,15 @@ else {
   employee.workTeacherTasks();
 }
 }
+
+type Subjects = "Math" | "History";
+function teachClass(todayClass : Subjects) : string {
+ if (todayClass === "Math") {
+  return "Teaching Math";
+ }
+  else {
+    return "Teaching History";
+  }
+}
+console.log(teachClass('Math'));
+console.log(teachClass('History'));
